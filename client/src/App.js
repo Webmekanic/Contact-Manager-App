eaf3b53ad2f53ac460f0a5cff82d71853900eb38
aniebodavid <aniebovictor001@gmail.com>
2022-11-15 06:25:21 -0800

    add contact context and global state

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import Navbar from "./components/layouts/Navbar"
 import Home from "./components/pages/Home"
 import About from "./components/pages/About"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import ContactState from "./context/contact/ContactState"
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   )
 }
 
