8dfc198f12ec9b55f5907b283b365f8155b744dd
aniebodavid <aniebovictor001@gmail.com>
2022-11-20 00:08:55 -0800

    Add contact from component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react"
-import uuid from "uuid"
+import { v4 as uuidv4 } from "uuid"
 import ContactContext from "./contactContext"
 import contactReducer from "./contactReducer"
 import {
@@ -42,6 +42,11 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState)
 
   // Add contact
+  const addContact = (contact) => {
+    contact.id = uuidv4()
+    dispatch({ type: "ADD_CONTACT", payload: contact })
+  }
+
   // DElete contact
   // Set current contact
   // clear current contact
@@ -53,6 +58,7 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        addContact,
       }}
     >
       {props.children}
