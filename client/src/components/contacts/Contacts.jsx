60d92981d68d232a5213cf86da05fddd1f04e3a7
aniebodavid <aniebovictor001@gmail.com>
2022-11-19 22:15:29 -0800

    Add contacts & contactItem components

---
+++ client/src/components/contacts/Contacts.jsx
@@ -0,0 +1,19 @@
+import React, { useContext, Fragment } from "react"
+import ContactContext from "../../context/contact/contactContext"
+import ContactsItem from "./ContactsItem"
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext)
+
+  const { contacts } = contactContext
+
+  return (
+    <Fragment>
+      {contacts.map((contact) => (
+        <ContactsItem key={contact.id} contact={contact} />
+      ))}
+    </Fragment>
+  )
+}
+
+export default Contacts
